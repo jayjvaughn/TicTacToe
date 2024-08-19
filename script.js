3549d0ccffd4b386f82103eb9a07034fa57e42e4
jayjvaughn <waveyjxyshop@gmail.com>
2024-08-19 00:36:11 -0400

    Added my box click functionality

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ function createGameBoard(){
         clickedBox = div1 = document.createElement("div");
         div1.classList.add(("box"));
         div1.setAttribute("id", boxID++);
+        div1.addEventListener("click", playerMarker);
         container.appendChild(div1);
+    };
+};
+let player1 = "X";
+let player2 = "O";
+function playerMarker(e){
+    clickedBox = e.target.id;
+if(div1.textContent === ""){
+    console.log(e.target.id);
+    e.target.textContent = player1;
+}
+else if(div1.textContent === "" && div1.textContent !== player1){
+    console.log(e.target.id);
+    e.target.textContent = player2;
     };
 };
 createGameBoard(); 
