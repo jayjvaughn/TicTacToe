cffa1b00a0d2278f6a0b7e5364dc40aa242b4e10
jayjvaughn <waveyjxyshop@gmail.com>
2024-08-20 18:58:13 -0400

    added a way to alternate from player 1 to player 2 via click counts

--- script.js
+++ script.js
@@ -12,24 +12,34 @@ function createGameBoard(){
         clickedBox = div1 = document.createElement("div");
         div1.classList.add(("box"));
         div1.setAttribute("id", boxID++);
-        div1.addEventListener("click", playerMarker);
+        div1.addEventListener("click", playRound);
         container.appendChild(div1);
     };
 };
 let player1 = "X";
 let player2 = "O";
-function playerMarker(e){
+let clicked = 0;
+function playRound(e){
     clickedBox = e.target.id;
-if(div1.textContent === ""){
-    console.log(e.target.id);
+if(div1.textContent === "" && clicked % 2 !== 0){
+    console.log(e.target.id + " was clicked");
     e.target.textContent = player1;
 }
-else if(div1.textContent === "" && div1.textContent !== player1){
-    console.log(e.target.id);
+else if(div1.textContent === "" && div1.textContent !== player1 && clicked % 2 === 0){
+    console.log(e.target.id + " was clicked");
     e.target.textContent = player2;
     };
 };
 createGameBoard(); 
+
+function clickCounter(){
+    container.addEventListener("click", ()=>{
+        clicked += 1;
+        console.log(clicked);
+        return clicked;
+    });
+};
+clickCounter();
 
 };
 ticTacToeGame();
